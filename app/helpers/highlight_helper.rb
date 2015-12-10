3664a118968a0399799d908bf8128bd980a6390b
Anton Dimitrov <dimitrov.anton@gmail.com>
2015-12-10 16:04:44 +0200

    Adds the ability to view the source code of students' own task runs. Also admins can view the source code for all task runs.

---
+++ app/helpers/highlight_helper.rb
@@ -0,0 +1,24 @@
+module HighlightHelper
+  def highlight_source_code(source_code, language)
+    formatter = Rouge::Formatters::HTML.new(css_class: 'highlight')
+    lexer = language_lexer(language)
+    formatter.format(lexer.lex(source_code))
+  end
+
+  protected
+
+  def language_lexer(language)
+    case language
+    when 'cpp'
+      Rouge::Lexers::Cpp.new
+    when 'python'
+      Rouge::Lexers::Python.new
+    when 'ruby'
+      Rouge::Lexers::Ruby.new
+    when 'java'
+      Rouge::Lexers::Java.new
+    else
+      Rouge::Lexers::PlainText.new
+    end
+  end
+end
