fa3a49f95966b57e2de6515ef88d4b86e81849b4
Anton Dimitrov <dimitrov.anton@gmail.com>
2016-04-18 23:33:31 +0300

    Stripping whitespace from user invitations.

--- app/controllers/user_invitations_controller.rb
+++ app/controllers/user_invitations_controller.rb
@@ -7,7 +7,8 @@ class UserInvitationsController < ApplicationController
   end
 
   def create
-    user_invitation = UserInvitation.create!(email: user_invitation_params[:email], used: false)
+    user_invitation = UserInvitation.create!(
+      email: user_invitation_params[:email].strip, used: false)
     redirect_to user_invitations_path, notice:
       "New user invitation created for #{ user_invitation_params[:email] }"
   end
@@ -15,7 +16,7 @@ class UserInvitationsController < ApplicationController
   def update
     user_invitation = UserInvitation.find(params[:id])
 
-    if user_invitation.update_attributes(email: user_invitation_params[:email])
+    if user_invitation.update_attributes(email: user_invitation_params[:email].strip)
       redirect_to user_invitations_path, notice:
         "Invitation for #{ user_invitation.email } updated successfully"
     else
