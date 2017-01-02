724bfaf55a2e28c3684d3bde33edffb7429fc117
Maksim Dimitrov <dimitrov.maksim@gmail.com>
2017-01-02 11:42:33 +0200

    Fixing quiz answer content migration to markdown and some code refactoring

--- db/migrate/20161221104754_transfer_question_content_to_markdown.rb
+++ db/migrate/20161221104754_transfer_question_content_to_markdown.rb
@@ -1,20 +1,20 @@
 class TransferQuestionContentToMarkdown < ActiveRecord::Migration
   def up
-    Quiz.find_each do |quiz|
-      quiz.quiz_questions.each do |quiz_question|
-        if quiz_question.content.present?
-          say "Updating content of quiz questions #{ quiz_question.id }"
-          quiz_question.update_attributes(markdown_content: quiz_question.content)
-        else
-          say "Error: empty quiz question content found!"
-        end
+    q
+    QuizQuestion.find_each do |quiz_question|
+      if quiz_question.content.present?
+        say "Updating content of quiz questions #{ quiz_question.id }"
+        quiz_question.update_attributes(markdown_content: quiz_question.content)
+      else
+        say "Error: empty quiz question content found!"
+      end
 
-        if quiz_question.explanation.present?
-          say "Updating explanation of quiz questions #{ quiz_question.id }"
-          quiz_question.update_attributes(markdown_explanation: quiz_question.explanation)
-        end
+      if quiz_question.explanation.present?
+        say "Updating explanation of quiz questions #{ quiz_question.id }"
+        quiz_question.update_attributes(markdown_explanation: quiz_question.explanation)
       end
     end
+
 
     change_column_null(:quiz_questions, :markdown_content, false)
   end
